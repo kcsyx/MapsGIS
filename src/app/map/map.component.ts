6548736b878daac96f420562ba691183b2c6dee7
Kieran <kieran.17@ichat.sp.edu.sg>
2021-10-28 14:03:45 +0800

    Added Traffic data layer, revised map generation code, and updated to set direction location of origin upon geolocating user

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,8 @@ import { environment } from '../../environments/environment';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import MapboxTraffic from '@mapbox/mapbox-gl-traffic';
+import { GeoJson, FeatureCollection } from '../map';
 
 @Component({
   selector: 'app-map',
@@ -10,39 +12,71 @@ import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-direct
 })
 
 export class MapComponent implements OnInit {
+
+  //map settings
   map: mapboxgl.Map;
   style = 'mapbox://styles/mapbox/streets-v11';
   lat = 1.346170;
   lng = 103.898630;
-  
+  // map data
+  source: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.initialiseMap();
+  }
+
+  private initialiseMap() {
+    // locate the user
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(position => {
+    //     this.lat = position.coords.latitude;
+    //     this.lng = position.coords.longitude;
+    //     this.map.flyTo({
+    //       center: [this.lng, this.lat]
+    //     })
+    //     // Add marker to center of map or user location after geolocating
+    //     const marker = new mapboxgl.Marker()
+    //       .setLngLat([this.lng, this.lat])
+    //       .addTo(this.map);
+    //   });
+    // };
+
+    this.buildMap();
+
+  }
+
+  buildMap() {
     Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set(environment.mapbox.accessToken);
     this.map = new mapboxgl.Map({
       container: 'map',
       style: this.style,
       zoom: 13,
       center: [this.lng, this.lat]
-    });    // Add map controls
+    });
+
+    // Add map controls
     this.map.addControl(new mapboxgl.NavigationControl());
-
-    this.map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true
-        },
-        // When active the map will receive updates to the device's location as it changes.
-        trackUserLocation: true,
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
-        showUserHeading: true
-      })
-    );
-
     var directions = new MapboxDirections({
       accessToken: environment.mapbox.accessToken
     });
-
     this.map.addControl(directions, 'top-left');
+    this.map.addControl(new MapboxTraffic());
+
+    // Initialize the GeolocateControl.
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    });
+    // Add the control to the map.
+    this.map.addControl(geolocate);
+    // Set an event listener that fires when a geolocate event occurs.
+    function success(pos) {
+      directions.setOrigin([pos.coords.longitude, pos.coords.latitude]);
+    }
+    geolocate.on('geolocate', success);
   }
 }
